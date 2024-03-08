1388d3813a983ecdc1959d05eac4b7b8cfce70bd
Adam Conner-Sax <adam_conner_sax@yahoo.com>
2024-03-08 13:02:48 -0500

    Cleaned up addJson

--- src/BlueRipple/Utilities/HvegaJsonData.hs
+++ src/BlueRipple/Utilities/HvegaJsonData.hs
@@ -10,17 +10,17 @@ import qualified Knit.Report as K
 import qualified Path
 import qualified System.Directory as SD
 
-brAddJSON :: K.KnitEffects r
-          => Path.Path a Path.Dir
+data JsonLocations a = JsonLocations { jsonDir :: Path.Path a Path.Dir, jsonUrl :: Text}
+
+addJSON :: K.KnitEffects r
+          => JsonLocations a
           -> Text
           -> A.Value
           -> K.Sem r ()
-brAddJSON destDir jsonName jsonVal = do
+addJSON jl jsonName jsonVal = do
+  let destDir = jsonDir jl
   K.liftKnit $ SD.createDirectoryIfMissing True (Path.toFilePath destDir)
   let jsonFileName = jsonName <> ".json"
       parseRel = first show . Path.parseRelFile . toString
   jsonPath' <- K.knitEither $ ((destDir Path.</>) <$> parseRel jsonFileName)
   K.liftKnit $ A.encodeFile (Path.toFilePath jsonPath') jsonVal
-
-
-data JsonLocations a = JsonLocations { jsonDir :: Path.Path a Path.Dir, jsonUrl :: Text}
